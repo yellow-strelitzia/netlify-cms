87d51f41d11b4a54b6e7faf30f66d3f1f65db22e
yellow-strelitzia <53438992+yellow-strelitzia@users.noreply.github.com>
2020-06-15 23:48:22 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = class extends React.Component {
           <img
             src={logo}
             alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            style={{ width: '22em', height: '10em' }}
           />
         </div>
         <div className="content has-text-centered has-background-black has-text-white-ter">
