c485d310b46c30e7003ced8398f22070f127e782
yellow-strelitzia <53438992+yellow-strelitzia@users.noreply.github.com>
2020-06-15 23:42:47 +0900

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,8 +40,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(226, 216, 116) 0.5rem 0px 0px, rgb(226, 216, 116) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(226, 216, 116)',
+              'rgb(250, 230, 50) 0.5rem 0px 0px, rgb(250, 230, 50) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(250, 230, 50)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
@@ -53,8 +53,8 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(226, 216, 116) 0.5rem 0px 0px, rgb(226, 216, 116) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(226, 216, 116)',
+              'rgb(250, 230, 50) 0.5rem 0px 0px, rgb(250, 230, 50) -0.5rem 0px 0px',
+            backgroundColor: 'rgb(250, 230, 50)',
             color: 'white',
             lineHeight: '1',
             padding: '0.25em',
