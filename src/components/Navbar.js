85e878cadace95260a2e2fd1837508c0a58616e9
yellow-strelitzia <53438992+yellow-strelitzia@users.noreply.github.com>
2020-06-15 18:18:40 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Yellow Strelitzia" style={{ width: '110px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -63,9 +63,11 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
+              <!--
               <Link className="navbar-item" to="/products">
                 Products
               </Link>
+              -->
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
